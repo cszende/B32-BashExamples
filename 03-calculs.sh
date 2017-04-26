2bf868549ffef5caeb45dcef8fb2e7f46bdb107b
cszende <cszende@gmail.com>
2017-04-26 18:03:20 -0400

    03 et 04 remplis et testés

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+#Mettre des parentheses et demander d'afficher la valeur obtenue ($)
+echo $((10+10))
+
+#Exemple invalide
+echo 10+10
+
+# read -p = Afficher la phrase avant d'insérer la valeur de la variable
+read -p "Nombre 1: " nb1
+read -p "Nombre 2: " nb2
+# let = Effectuer un calcul avec les valeurs ($) SANS ESPACE
+let resultat=$nb1+$nb2
+echo  $resultat
+
+
--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,51 @@
 #!/bin/bash
 
+#Initialisation d'une variable ***Sans espace
+note=60
+
+
+if test $note -lt 60
+then
+	echo "You failed miserably"
+#traduction:
+#si le test sur la valeur de note ($note) est inferieure (-lt) a  60
+#alors (then)
+#affichage d'une phrase
+
+
+elif test $note -eq 60
+then
+	echo "ouf! c'est juste!"
+#traduction:
+#else if (elif) le test sur $note est égal a 60
+#alors affichage d'une autre phrase
+
+
+else 
+	echo "Alright yeah yeah yeah!"
+fi
+#traduction:
+#sinon, affichage d'une autre phrase
+# fi = fin de l'instruction IF
 
 
 
+read -p "Entrez une lettre: " lettre
 
+case $lettre in		#si $lettre est
+	[[:lower:]])	#un carac minuscule
+		echo "lettre en minuscule"
+		;;	# break;
+	*)		#pour tout autre cas
+		echo "Majuscule"
+		;;	# break;
+esac			#Sortir de l'instruction CASE
+
+
+if test $lettre != "a"	#si lettre n'est pas a
+then
+	echo "La lettre n'est pas a"
+fi
 
 
 
