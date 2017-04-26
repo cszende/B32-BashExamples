40b4ce2e2d4c49262473ba8e7e37ae30d18c4a15
cszende <cszende@gmail.com>
2017-04-26 17:41:42 -0400

    01 et 02 remplis et teste

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+#script tres simple qui affiche Salut a l'utilisateur courant
+
+echo Salut $USER
+
+# $=Valeur de la variable
--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+echo "salut"
+#exit - donc retour de ligne
+echo "toi"
+
+# -n = pas de retour de ligne
+echo -n "Vive"
+echo " Linux"
+
+# -e = Accepter les méta-caracteres (\n, \t, etc...)
+echo -e "1\n2\n3"
+
+# -p = une phrase sera demandee a l'utilisateur
+# sans $ devant une variable: on affecte la variable
+read -p "Quel âge avez-vous?" age
+echo $age "ans! Vous paraissez jeune!"
+
