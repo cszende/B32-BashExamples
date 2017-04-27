facb372f11604da776b2858b7c16e3da03f416a3
cszende <cszende@gmail.com>
2017-04-27 08:43:37 -0400

    09 rempli et testé

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+#script d'un programme qui dit si un fichier existe (-d pour repertoire)
+#écrire le nom du programme suivi du nom du fichier a verifier
+
+if test $# -ge 1			# -ge = greater or equal
+then					# $# = Nb de parametres passes au programme
+	if test -e $1			# -e = vrai si le fichier existe
+	then
+		echo "Il existe"
+	fi
+fi
