a43c12f5d3fe1e1da132156b72f773304aa7fc23
cszende <cszende@gmail.com>
2017-04-27 08:26:45 -0400

    07 rempli et testé

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+
+
+maFonction() {
+	echo "salut $1" 
+}
+# $1 = prendre 1 parametre lors de l'appel de la fonction
+
+
+#Pas besoin de parametre () lors de l'appel
+maFonction Marco
+maFonction JS
+maFonction "TOI LA LA"
+
+
