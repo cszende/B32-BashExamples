f4094d42ea753259ecfd50b2ee0427249f27142c
cszende <cszende@gmail.com>
2017-04-27 08:33:49 -0400

    08 rempli et testé

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+#Expressions regulieres
+
+
+# Version lonngue de "read -p"
+echo -n "Lettres: "
+read lettres
+
+
+#Version longue de "test" = [[ ]]
+# ~ = regexp
+if [[ $lettres =~ ^[a-zA-Z]+$ ]]
+then
+	echo "C'est un mot"
+fi
+
+
+
