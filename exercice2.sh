850536223ae2997b8366fad40b1376d12c9b6c17
cszende <cszende@gmail.com>
2017-04-27 09:26:46 -0400

    exercice2 rempli et testé

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+choix="0"
+
+AfficherMenu() {
+	clear
+	echo -e "MENU\nA. Non\nB. Non plus\nC. Ca sert a rien\nQ. Quitter"
+}
+
+while test $choix != "q" #***Attention: mettre les espaces!
+do
+	AfficherMenu
+	read -p "Votre choix: " choix
+done
+
